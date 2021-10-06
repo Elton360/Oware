7478c9ba22a16520cffd952285379633b10b06c0
Elton360 <elton.lucien1@gmail.com>
2021-10-06 11:29:44 -0400

    UI Complete

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ const overlay = document.querySelector(".overlay");
 const begin = document.querySelector(".begin");
 const game = document.querySelector(".game");
 const hole1 = document.querySelector(".holes-1");
-const hole2 = document.querySelector(".holes-2");
-const holeImg = document.getElementsByClassName(".hole-img");
-
-console.log(hole1);
+let hole2 = document.querySelector(".holes-2");
+const restart = document.querySelector(".restart");
+const home = document.querySelector(".home");
 
 //Player 1 (Pink)
 const holes1 = [4, 4, 4, 4, 4, 4];
@@ -21,14 +20,34 @@ const displayHoles = function (player1Arr, player2Arr) {
   hole1.innerHTML = hole2.innerHTML = "";
 
   player1Arr.forEach((dots, i) => {
-    const html = `<img class = hole-img src="./images/holes-pink/pink-${dots}.png" alt="">`;
+    const html = `
+    <div class = "playImg playImg2 playOrig">
+      <div class = "playPass no-opacity">
+        <h1 class = "play">Play</h1>
+        <h1 class = "drop">Drop</h1>
+      </div>
+      <div class ="hole">
+        <img class = "hole-img pink-hole"  src="./images/holes-pink/pink-${dots}.png" alt="">
+      </div>
+    </div>`;
     hole1.insertAdjacentHTML("afterbegin", html);
   });
 
   player2Arr.forEach((dots, i) => {
-    const html = `<img class = hole-img src="./images/holes-white/white-${dots}.png" alt="">`;
+    const html = `
+    <div class = "playImg playImg1">
+      <div class = "playPass no-opacity">
+        <h1 class = "play">Play</h1>
+        <h1 class = "drop">Drop</h1>
+      </div>
+      <div class ="hole ">
+        <img class = "hole-img white-hole"  src="./images/holes-white/white-${dots}.png" alt="">
+      </div>
+    </div>`;
     hole2.insertAdjacentHTML("afterbegin", html);
   });
+
+  holeImg = document.querySelectorAll(".hole-img");
 };
 
 //Starting Conditions
@@ -49,7 +68,15 @@ begin.addEventListener("click", function (e) {
   e.preventDefault();
   clearInterval(throb);
   begin.style.display = "none";
-  overlay.style.opacity = "0";
-  holeImg.style.margin = "1rem";
+  overlay.style.display = "none";
+  restart.style.opacity = "1";
+  home.style.opacity = "1";
   displayHoles(holes1, holes2);
+
+  holeImg.forEach((img, i) => {
+    img.addEventListener("click", (e) => {
+      console.log(img.parentElement.previousSibling.previousSibling);
+      img.parentElement.previousSibling.previousSibling.style.opacity = "1";
+    });
+  });
 });
